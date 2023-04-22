3924185d876e27f2a193ecf2d9a9514b149d7f41
Sinrok Kim <fresh502@gmail.com>
2023-04-22 17:05:31 +0900

    ch-2 4,5

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,18 +3,22 @@ import { ApiExtraModels, ApiTags } from '@nestjs/swagger';
 import { UserService } from './user.service';
 import { FindUserReqDto } from './dto/req.dto';
 import { PageReqDto } from 'src/common/dto/req.dto';
+import { ApiGetItemsResponse, ApiGetResponse } from 'src/common/decorator/swagger.decorator';
+import { FindUserResDto } from './dto/res.dto';
 
 @ApiTags('User')
-@ApiExtraModels(FindUserReqDto)
+@ApiExtraModels(FindUserReqDto, FindUserResDto)
 @Controller('api/users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @ApiGetItemsResponse(FindUserResDto)
   @Get()
   findAll(@Query() { page, size }: PageReqDto) {
     return this.userService.findAll();
   }
 
+  @ApiGetResponse(FindUserResDto)
   @Get(':id')
   findOne(@Param() { id }: FindUserReqDto) {
     return this.userService.findOne(id);
