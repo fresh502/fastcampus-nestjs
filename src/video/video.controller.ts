a25089d7cfaab525dacee58f26a40c01bd86a777
Sinrok Kim <fresh502@gmail.com>
2023-04-22 17:05:31 +0900

    ch-2 4,5

--- src/video/video.controller.ts
+++ src/video/video.controller.ts
@@ -3,23 +3,29 @@ import { ApiExtraModels, ApiTags } from '@nestjs/swagger';
 import { VideoService } from './video.service';
 import { CreateVideoReqDto, FindVideoReqDto } from './dto/req.dto';
 import { PageReqDto } from 'src/common/dto/req.dto';
+import { ApiGetItemsResponse, ApiGetResponse, ApiPostResponse } from 'src/common/decorator/swagger.decorator';
+import { CreateVideoResDto, FindVideoResDto } from './dto/res.dto';
+import { PageResDto } from 'src/common/dto/res.dto';
 
 @ApiTags('Video')
-@ApiExtraModels(FindVideoReqDto, PageReqDto)
+@ApiExtraModels(FindVideoReqDto, PageReqDto, CreateVideoResDto, FindVideoResDto, PageResDto)
 @Controller('api/videos')
 export class VideoController {
   constructor(private readonly videoService: VideoService) {}
 
+  @ApiPostResponse(CreateVideoResDto)
   @Post()
   upload(@Body() createVideoReqDto: CreateVideoReqDto) {
     return this.videoService.create();
   }
 
+  @ApiGetItemsResponse(FindVideoResDto)
   @Get()
   findAll(@Query() { page, size }: PageReqDto) {
     return this.videoService.findAll();
   }
 
+  @ApiGetResponse(FindVideoResDto)
   @Get(':id')
   findOne(@Param() { id }: FindVideoReqDto) {
     return this.videoService.findOne(id);
