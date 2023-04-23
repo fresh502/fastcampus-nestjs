eef0678c793f359ef08708c53e8d367220adcb31
Sinrok Kim <fresh502@gmail.com>
2023-04-23 12:54:14 +0900

    ch-5 1

---
+++ src/auth/entity/refresh-token.entity.ts
@@ -0,0 +1,30 @@
+import { User } from 'src/user/entity/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class RefreshToken {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  token: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  @OneToOne(() => User, (user) => user.refreshToken)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+}
