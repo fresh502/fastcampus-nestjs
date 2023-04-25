22deef8acd505c59f5cec530b5d7fd88e283b009
Sinrok Kim <fresh502@gmail.com>
2023-04-25 22:38:40 +0900

    ch-5 2

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Request, Post, Body, BadRequestException } from '@nestjs/common';
+import { Controller, Post, Body, Headers, BadRequestException } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { ApiCreatedResponse, ApiExtraModels, ApiTags, getSchemaPath } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiExtraModels, ApiTags } from '@nestjs/swagger';
 import { SigninReqDto, SignupReqDto } from './dto/req.dto';
-import { SigninResDto, SignupResDto } from './dto/res.dto';
+import { RefreshResDto, SigninResDto, SignupResDto } from './dto/res.dto';
 import { ApiPostResponse } from 'src/common/decorator/swagger.decorator';
 import { Public } from 'src/common/decorator/public.decorator';
+import { User } from 'src/common/decorator/user.decorator';
+import { UserAfterAuth } from 'src/common/decorator/user.decorator';
 
 @ApiTags('Auth')
-@ApiExtraModels(SignupResDto, SigninResDto)
+@ApiExtraModels(SignupResDto, SigninResDto, RefreshResDto)
 @Controller('api/auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
@@ -26,5 +28,14 @@ export class AuthController {
   @Post('signin')
   async signin(@Body() { email, password }: SigninReqDto) {
     return this.authService.signin(email, password);
+  }
+
+  @ApiPostResponse(RefreshResDto)
+  @ApiBearerAuth()
+  @Post('refresh')
+  async refresh(@Headers('authorization') authorization, @User() user: UserAfterAuth) {
+    const token = /Bearer\s(.+)/.exec(authorization)[1];
+    const { accessToken, refreshToken } = await this.authService.refresh(token, user.id);
+    return { accessToken, refreshToken };
   }
 }
