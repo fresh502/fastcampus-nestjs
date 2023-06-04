d3af8fd3313f13df70e13879351963f8afd01607
Sinrok Kim <fresh502@gmail.com>
2023-06-04 10:03:36 +0900

    ch-9 1

--- src/video/video.service.ts
+++ src/video/video.service.ts
@@ -1,16 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { Video } from './entity/video.entity';
 
 @Injectable()
 export class VideoService {
-  constructor(@InjectRepository(Video) private videoRepository: Repository<Video>) {}
-
-  async create() {
-    return 'create';
-  }
-
   async findAll() {
     return 'video list';
   }
